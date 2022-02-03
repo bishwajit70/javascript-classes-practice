520882a9ad2d04c1d555513854ff9040be0110ac
bishwajit70 <mailtobishwa15@gmail.com>
2022-02-03 15:58:38 +0600

    Lowest Number Calculation Function

---
+++ lowest-number-of-array.js
@@ -0,0 +1,14 @@
+// Find lowest Number of an array function 
+
+function lowestNumber(myNumbers) {
+    let lowestNumber = myNumbers[0];
+    for (const element of myNumbers) {
+        if (element < lowestNumber) {
+            lowestNumber = element;
+        }
+    }
+    return lowestNumber;
+}
+let myNumbers = [19, 30, 40, 58, 53, 70, 20, 18, 12, 85];
+let myLowestNumber = lowestNumber(myNumbers);
+console.log(myLowestNumber);
